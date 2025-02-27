27fe29393344b6a82d987584edafa153fb8e9c68
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-27 07:19:12 +0700

    Update play.js

--- system/plugins/downloader/play.js
+++ system/plugins/downloader/play.js
@@ -1,105 +1,138 @@
+const dist = require('@distube/ytdl-core')
 const yts = require('yt-search')
-const ytdl = require('@distube/ytdl-core')
+const fetch = require("node-fetch");
 
-class Command {
-    constructor() {
-        this.command = "play"
-        this.alias = []
-        this.category = ["downloader"]
-        this.settings = {}
-        this.description = "Mengsearch/Link Video/Music Dari Yt"
-        this.loading = true
-    }
-    run = async (m, {
+let rinokumura = {
+    command: "play",
+    alias: [
+        "ply",
+        "playvd",
+        "playms"
+    ],
+    category: [
+        "downloader"
+    ],
+    settings: {
+        limit: true
+    },
+    loading: true,
+    async run(m, {
         sock,
+        client,
+        conn,
+        DekuGanz,
         Func,
         Scraper,
-        config,
-        store,
-        text
-    }) => {
-        if (!text) throw `\`[ Contoh ]\` ${m.prefix + m.command} Opening Boku no Hero academia`
-        if (/youtube.com/.test(text)) {
-            const id = await ytdl.getVideoID(text)
-            let result = await yts({
-                videoId: id,
-                hl: 'id',
-                gl: 'ID'
-            })
-            if (result === 0) {
-                m.reply('maaf ga dapat di convert...')
-            }
-            let deku = `⏤͟͟͞͞╳── *[ ᴘʟᴀʏ - ʏᴏᴜᴛᴜʙᴇ ]* ── .々─ᯤ\n`
-            deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
-            deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
-            deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
-            deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
-            deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
-            deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
-            deku += `⏤͟͟͞͞╳────────── .✦`
-            await m.reply({
-                image: {
-                    url: result.thumbnail
-                },
-                caption: deku,
-                footer: config.ownername,
-                viewOnce: true,
-                headerType: 6,
-                buttons: [{
-                    buttonId: `.ytmp4 ${result.url}`,
-                    buttonText: {
-                        displayText: 'Video'
-                    }
-                }, {
-                    buttonId: `.ytmp3 ${result.url}`,
-                    buttonText: {
-                        displayText: "Audio"
-                    }
-                }]
-            })
+        text,
+        config
+    }) {
+        client.yt = client.yt || {}
+        if (!text) throw '⚠️ mana? link/query nya?'
+        let isAudio = text.includes("--audio")
+        let isVideo = text.includes("--video")
+
+        let videoUrl;
+        if (text.startsWith("http")) {
+            videoUrl = text;
         } else {
-            let convert = await yts({
-                search: text,
-                hl: 'id',
-                gl: 'ID'
-            })
-            let result = convert.all[0]
-
-            if (result === 0) {
-                m.reply('maaf ga ketemu...')
-            }
-
-            let hah = result.url;
-            let deku = `⏤͟͟͞͞╳── *[ ᴘʟᴀʏ - ʏᴏᴜᴛᴜʙᴇ ]* ── .々─ᯤ\n`
-            deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
-            deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
-            deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
-            deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
-            deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
-            deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
-            deku += `⏤͟͟͞͞╳────────── .✦`
-            await m.reply({
-                image: {
-                    url: result.thumbnail
-                },
-                caption: deku,
-                footer: config.ownername,
-                viewOnce: true,
-                headerType: 6,
-                buttons: [{
-                    buttonId: `.ytmp4 ${result.url}`,
-                    buttonText: {
-                        displayText: 'Video'
-                    }
-                }, {
-                    buttonId: `.ytmp3 ${result.url}`,
-                    buttonText: {
-                        displayText: "Audio"
-                    }
-                }]
-            })
+            let search = await yts(text);
+            if (!search.videos.length) return m.reply("❌ Video tidak ditemukan!");
+            videoUrl = search.videos[0].url;
         }
+
+        client.yt[m.sender] = {
+            url: videoUrl
+        };
+
+        const getid = await dist.getVideoID(client.yt[m.sender].url || videoUrl)
+
+        const getmetadata = await yts({
+            videoId: getid,
+            hl: 'id',
+            gl: 'ID'
+        });
+        const UrlYt = client.yt[m.sender].url || videoUrl
+        let metadata = `> • Title: ${getmetadata.title}
+> • Id: ${getmetadata.videoId}
+> • Ago: ${getmetadata.ago}
+> • Durasi: ${getmetadata.timestamp}
+> • Url: ${getmetadata.url}
+`
+        let infoMessage = `📁 Download YouTube
+${metadata}
+
+ - Pilih Mau Yang mana !
+ - 1 video download 
+ - 2 audio download
+`
+
+        if (!isAudio && !isVideo) {
+            await client.sendAliasMessage(m.cht, {
+                text: infoMessage
+            }, [{
+                alias: `1`,
+                response: `${m.prefix + m.command} ${UrlYt} --video`
+            }, {
+                alias: `2`,
+                response: `${m.prefix + m.command} ${UrlYt} --audio`
+            }], m);
+        }
+
+        const finalUrl = client.yt[m.sender].url || videoUrl
+
+        if (isAudio) {
+            const audio = await Scraper.SaveTube.download(finalUrl, "mp3")
+            const sizea = await Func.getSize(audio.result.download)
+            if (audio.size > 10 * 1024 * 1024) {
+                return sock.sendMessage(m.cht, {
+                    document: {
+                        url: audio.result.download
+                    },
+                    mimetype: "audio/mpeg",
+                    fileName: `${audio.result.title}.mp3`,
+                }, {
+                    quoted: m
+                });
+            } else {
+                return sock.sendMessage(m.cht, {
+                    audio: {
+                        url: audio.result.download
+                    },
+                    mimetype: "audio/mpeg",
+                }, {
+                    quoted: m
+                });
+            }
+        } else if (isVideo) {
+            let video = await Scraper.SaveTube.download(finalUrl, "720")
+            let response = await fetch(video.result.download, {
+                method: "HEAD"
+            });
+            let fileSizeInBytes = parseInt(response.headers.get("content-length"));
+            if (fileSizeInBytes > 10 * 1024 * 1024) {
+                return sock.sendMessage(m.cht, {
+                    document: {
+                        url: video.result.download
+                    },
+                    mimetype: "video/mp4",
+                    fileName: `${video.result.title}.mp4`,
+                }, {
+                    quoted: m
+                });
+            } else {
+                return sock.sendMessage(m.cht, {
+                    video: {
+                        url: video.result.download
+                    },
+                    mimetype: "video/mp4",
+                    caption: `📁 YouTube Dl Video\n${metadata}`
+                }, {
+                    quoted: m
+                });
+            }
+        }
+        setTimeout(() => delete sock.yt[m.sender], 5000);
     }
 }
 
-module.exports = new Command();
+module.exports = rinokumura
