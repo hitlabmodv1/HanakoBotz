f1331b078efa57b01b83a28b69a78617c118bf13
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-03-02 10:15:12 +0700

    Update toesm.js

--- system/plugins/tools/toesm.js
+++ system/plugins/tools/toesm.js
@@ -1,17 +1,27 @@
-module.exports = {
-  command: "toesm",
-  alias: [ ],
-  category: ["tools"],
-  settings: {
-    limit: true,
-  },
-  description: "convert ke esm",
-  loading: true,
-  async run(m, { text, sock, Scraper, Func }) {
- const args = m.args
- let codeToConvert = text || (m.quoted && m.quoted.text);
+const bft = require('js-beautify')
 
-    if (!codeToConvert) return m.reply(`Masukkan atau reply kode yang ingin diubah`);
+module.exports = {
+    command: "toesm",
+    alias: [],
+    category: ["tools"],
+    settings: {
+        limit: true,
+    },
+    description: "convert ke esm",
+    loading: true,
+    async run(m, {
+        text,
+        sock,
+        Scraper,
+        Func
+    }) {
+        let quoted = m.quoted ? m.quoted : m;
+        if (!quoted) return m.reply(`Masukkan atau reply kode yang ingin diubah`);
+        const es = await convertCJSToESM(quoted.body)
+        const result = await bft(es)
+        m.reply(result);
+    },
+};
 
 function convertCJSToESM(code) {
     return code
@@ -22,31 +32,3 @@ function convertCJSToESM(code) {
         .replace(/exports\.(\w+)\s*=\s*(.*?);?/g, 'export const $1 = $2;')
         .replace(/require\(['"](.+?)['"]\)/g, 'await import(\'$1\')'); // Menangani dynamic imports
 }
-
-function convertESMToCJS(code) {
-    return code
-        .replace(/import (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
-        .replace(/import \* as (\w+) from ['"](.+?)['"];/g, 'const $1 = require(\'$2\');')
-        .replace(/import \{(.*?)\} from ['"](.+?)['"];/g, (match, p1, p2) => {
-            const imports = p1.split(',').map(i => i.trim());
-            return `const { ${imports.join(', ')} } = require('${p2}');`;
-        })
-        .replace(/export default (\w+);?/g, 'module.exports = $1;')
-        .replace(/export const (\w+) = (\w+);?/g, 'exports.$1 = $2;')
-        .replace(/export (.*?) from ['"](.+?)['"];/g, (match, p1, p2) => {
-            return `module.exports.${p1} = require('${p2}');`;
-        }); // Menangani re-export
-}
-
-    let result;
-    if (args[0] === '--esm') {
-        result = convertCJSToESM(codeToConvert);
-    } else if (args[0] === '--cjs') {
-        result = convertESMToCJS(codeToConvert);
-    } else {
-        return m.reply(`Perintah tidak dikenal`);
-    }
-
-    m.reply(result);
-  },
-};
