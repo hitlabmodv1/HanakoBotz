612f3141396c95eb4f6198d9b419d446f3539438
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-03-02 10:08:09 +0700

    Update play.js

--- system/plugins/downloader/play.js
+++ system/plugins/downloader/play.js
@@ -91,6 +91,13 @@ ${metadata}
                 audio = ytdown.result;
             }
 
+            const getid = await dist.getVideoID(finalUrl);
+            const audiom = await yts({
+                videoId: getid,
+                hl: 'id',
+                gl: 'ID'
+            });
+
             const sizea = await Func.getSize(audio)
             if (sizea > 10 * 1024 * 1024) {
                 return sock.sendMessage(m.cht, {
@@ -98,7 +105,7 @@ ${metadata}
                         url: audio
                     },
                     mimetype: "audio/mpeg",
-                    fileName: `${audio.result.title}.mp3`,
+                    fileName: `${audiom.title}.mp3`,
                 }, {
                     quoted: m
                 });
@@ -114,6 +121,13 @@ ${metadata}
             }
         } else if (isVideo) {
             const savetubev = await Scraper.SaveTube.download(finalUrl, "720")
+
+            const getid = await dist.getVideoID(finalUrl);
+            const videom = await yts({
+                videoId: getid,
+                hl: 'id',
+                gl: 'ID'
+            });
 
             let video;
             if (savetubev.result.download) {
@@ -133,7 +147,7 @@ ${metadata}
                         url: video
                     },
                     mimetype: "video/mp4",
-                    fileName: `${video.result.title}.mp4`,
+                    fileName: `${videom.title}.mp4`,
                 }, {
                     quoted: m
                 });
@@ -143,7 +157,7 @@ ${metadata}
                         url: video
                     },
                     mimetype: "video/mp4",
-                    caption: `📁 YouTube Dl Video\n${metadata}`
+                    caption: `📁 Download YouTube\n${metadata}`
                 }, {
                     quoted: m
                 });
