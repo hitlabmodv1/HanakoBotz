5e6c762bd6d3c9398990facb5af8258c6865b5b6
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-03-06 00:39:55 +0700

    Update _afk-detect.js

--- system/plugins/events/_afk-detect.js
+++ system/plugins/events/_afk-detect.js
@@ -15,16 +15,6 @@ async function events(m, {
                 text: `Jangan tag ${m.pushName}, Dia sedang AFK selama: *${Func.toTime(new Date - aefka)}*\n\n➠ *Reason:* ${reason ? reason : '-'}`,
             })
         }
-        if (db.list().user[m.sender].afk.afkTime > -1) {
-            let is_user = db.list().user[m.sender].afk
-            if (!m.isGroup) return
-            let aefka = is_user.afkTime
-            let reason = is_user.afkReason || ''
-            db.list().user[m.sender].afk.afkTime = -1
-            m.reply({
-                text: `Anda Kembali ${m.pushName}, Afk selama: *${Func.toTime(new Date - aefka)}*\n\n➠ *Reason:* ${reason ? reason : '-'}`,
-            })
-        }
     } catch (e) {
         throw e;
     }
