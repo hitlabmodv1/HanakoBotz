ec87594535822e1944fdf51df5da170d340f0f25
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-03-06 06:31:45 +0700

    Update _afk-detect.js

--- system/plugins/events/_afk-detect.js
+++ system/plugins/events/_afk-detect.js
@@ -3,17 +3,17 @@ async function events(m, {
     Func
 }) {
     try {
-        let afk = [...new Set([...(m.mentions || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-        for (let jid of afk) {
+        let mentions = [...new Set([...(m.mentions || []), ...(m.quoted ? [m.quoted.sender] : [])])]
+        for (let jid of mentions) {
             let is_user = db.list().user[jid].afk
             if (!is_user) continue
             if (!m.isGroup) return
             let aefka = is_user.afkTime
             if (!aefka || aefka < 0) continue
             let reason = is_user.afkReason || ''
-            m.reply({
-                text: `Jangan tag ${m.pushName}, Dia sedang AFK selama: *${Func.toTime(new Date - aefka)}*\n\n➠ *Reason:* ${reason ? reason : '-'}`,
-            })
+            if (!m.fromMe) {
+                m.reply(`Jangan tag @${jid.split('@')[0]}, Dia sedang AFK selama: *${Func.toTime(new Date - aefka)}*\n\n➠ *Reason:* ${reason ? reason : '-'}`)
+            }
         }
     } catch (e) {
         throw e;
