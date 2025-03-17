ff2d56848bcc66ef3467e7fd30119e25fd4c002c
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-03-18 00:02:23 +0700

    Update play.js

--- system/plugins/downloader/play.js
+++ system/plugins/downloader/play.js
@@ -1,6 +1,4 @@
-const dist = require('@distube/ytdl-core')
 const yts = require('yt-search')
-const fetch = require("node-fetch");
 
 let rinokumura = {
     command: "play",
@@ -26,30 +24,12 @@ let rinokumura = {
         text,
         config
     }) {
-        client.yt = client.yt || {}
-        if (!text) throw '⚠️ Mau Query/Link?'
-        let isAudio = text.includes("--audio")
-        let isVideo = text.includes("--video")
+        if (!text) throw '⚠️ Mau Query Song Apa?'
 
-        let videoUrl;
-        if (text.startsWith("http")) {
-            const getid = await dist.getVideoID(text);
-            video = await yts({
-                videoId: getid,
-                hl: 'id',
-                gl: 'ID'
-            });
-        } else {
-            let search = await yts(text);
-            if (!search.videos.length) return m.reply("❌ Video tidak ditemukan!");
-            video = search.videos[0];
-        }
+        let search = await yts(text);
+        if (!search.videos.length) return m.reply("❌ Video tidak ditemukan!");
+        const video = search.videos[0];
 
-        client.yt[m.sender] = {
-            url: video.url
-        };
-
-        const UrlYt = client.yt[m.sender].url || video.url
         let metadata = `> • Title: ${video.title}
 > • Id: ${video.videoId}
 > • Ago: ${video.ago}
@@ -63,110 +43,18 @@ ${metadata}
 > • 1. video download 
 > • 2. audio download`
 
-        if (!isAudio && !isVideo) {
-            await client.sendAliasMessage(m.cht, {
-                image: {
-                    url: video.thumbnail
-                },
-                caption: infoMessage
-            }, [{
-                alias: `1`,
-                response: `${m.prefix + m.command} ${UrlYt} --video`
-            }, {
-                alias: `2`,
-                response: `${m.prefix + m.command} ${UrlYt} --audio`
-            }], m);
-        }
-
-        const finalUrl = client.yt[m.sender].url || video.url
-
-        if (isAudio) {
-            let audio;
-            try {
-                const { result: savetube } = await Scraper.SaveTube(finalUrl, "mp3")
-                audio = savetube.download;
-            } catch (e) {
-                try {
-                    const ytdowna = await Scraper.ytdown(finalUrl, "mp4", 720)
-                    audio = ytdown.result;
-                } catch (e) {}
-            }
-
-            const getid = await dist.getVideoID(finalUrl);
-            const audiom = await yts({
-                videoId: getid,
-                hl: 'id',
-                gl: 'ID'
-            });
-
-            const sizea = await Func.getSize(audio)
-            if (sizea > 10 * 1024 * 1024) {
-                return sock.sendMessage(m.cht, {
-                    document: {
-                        url: audio
-                    },
-                    mimetype: "audio/mpeg",
-                    fileName: `${audiom.title}.mp3`,
-                }, {
-                    quoted: m
-                });
-            } else {
-                return sock.sendMessage(m.cht, {
-                    audio: {
-                        url: audio
-                    },
-                    mimetype: "audio/mpeg",
-                }, {
-                    quoted: m
-                });
-            }
-        } else if (isVideo) {
-
-            const getid = await dist.getVideoID(finalUrl);
-            const videom = await yts({
-                videoId: getid,
-                hl: 'id',
-                gl: 'ID'
-            });
-
-            let video;
-            try {
-                const { result: savetube } = await Scraper.SaveTube(finalUrl, "720")
-                video = savetube.download;
-            } catch (e) {
-                try {
-                    const ytdownv = await Scraper.ytdown(finalUrl, "mp4", 720)
-                    video = ytdownv.result;
-                } catch (e) {}
-            }
-
-            let response = await fetch(video, {
-                method: "HEAD"
-            });
-            let fileSizeInBytes = parseInt(response.headers.get("content-length"));
-            if (fileSizeInBytes > 10 * 1024 * 1024) {
-                return sock.sendMessage(m.cht, {
-                    document: {
-                        url: video
-                    },
-                    mimetype: "video/mp4",
-                    fileName: `${videom.title}.mp4`,
-                }, {
-                    quoted: m
-                });
-            } else {
-                return sock.sendMessage(m.cht, {
-                    video: {
-                        url: video
-                    },
-                    mimetype: "video/mp4",
-                    caption: `📁 Download YouTube\n${metadata}`
-                }, {
-                    quoted: m
-                });
-            }
-        }
-        setTimeout(() => delete sock.yt[m.sender], 5000);
+        await client.sendAliasMessage(m.cht, {
+            image: {
+                url: video.thumbnail
+            },
+            caption: infoMessage
+        }, [{
+            alias: `1`,
+            response: `${m.prefix}ytdl ${video.url} --video`
+        }, {
+            alias: `2`,
+            response: `${m.prefix}ytdl ${video.url} --audio`
+        }], await m.froll());
     }
 }
 
